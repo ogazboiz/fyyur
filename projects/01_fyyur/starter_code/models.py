8bc820159bcc416e7e95be43cb08f149912c4f50
Akpolo Prince <ogazboizakpolo@gmail.com>
2022-07-31 00:59:26 +0100

    done with the sreating artist

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------#
 from app import db
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
@@ -17,7 +17,7 @@ class Venue(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
